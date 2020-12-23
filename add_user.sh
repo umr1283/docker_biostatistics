5d3855bf1826f127fcc17d20bcdc139317a79939
Mickaël Canouil <mickael.canouil@cnrs.fr>
2020-12-23 16:07:04 +0100

    initialise repo

---
+++ add_user.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+USER=$1
+ID=$2
+STATUS=$3
+
+if [ -z $STATUS ] || [ $STATUS = "normal" ]
+then
+
+  if [ -d /media/User/$USER ]
+  then
+    useradd \
+    --no-create-home \
+    --no-user-group \
+    --gid staff \
+    --uid $ID \
+    --home /media/User/$USER \
+    --groups staff,root,sudo \
+    $USER &&
+    echo "$USER:$USER" | chpasswd
+  else
+    useradd \
+    --create-home \
+    --no-user-group \
+    --gid staff \
+    --uid $ID \
+    --home /media/User/$USER \
+    --groups staff,root,sudo \
+    $USER &&
+    echo "$USER:$USER" | chpasswd
+  fi
+
+else
+
+  if [ -d /media/User/$USER ]
+  then
+    useradd \
+    --no-create-home \
+    --no-user-group \
+    --gid staff \
+    --uid $ID \
+    --home /media/User/$USER \
+    --groups staff \
+    $USER &&
+    echo "$USER:$USER" | chpasswd
+  else
+    useradd \
+    --create-home \
+    --no-user-group \
+    --gid staff \
+    --uid $ID \
+    --home /media/User/$USER \
+    --groups staff \
+    $USER &&
+    echo "$USER:$USER" | chpasswd
+  fi
+    
+fi
+
+[ -f /media/User/$USER/.bash_profile ] || echo '
+# .bash_profile
+
+umask u=rwx,g=rwx,o=
+
+### ================================================================================================
+### Set locales
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_PAPER="en_US.UTF-8"
+export LC_NAME="en_US.UTF-8"
+export LC_ADDRESS="en_US.UTF-8"
+export LC_TELEPHONE="en_US.UTF-8"
+export LC_MEASUREMENT="en_US.UTF-8"
+export LC_IDENTIFICATION="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+
+### ================================================================================================
+### Set prompt
+export PS1="________________________________________________________________________________\n| \w @ \h (\u) \n| > "
+export PS2="| > "
+export EDITOR=/usr/bin/nano
+export BLOCKSIZE=1k
+
+
+### ================================================================================================
+### Set aliases
+alias R="R --no-save --no-restore-data --no-init-file"
+alias Rscript="Rscript --no-init-file"
+
+alias gaa="git add --all"
+alias gam="git commit -am"
+alias gm="git commit -m"
+alias gp="git push"
+alias gss="git status"
+
+alias cp="cp -iv"                           # Nouvelle copie
+alias mv="mv -iv"                           # Nouveau move
+alias mkdir="mkdir -pv"                     # Nouvelle création de dossier
+alias ll="ls -FlAhp --color=auto"           # Affiche fichier, dossier, et fichiers cachés
+alias l="ls -Flhp --color=auto"             # Affiche fichier et dossier
+alias ls="ls --color=auto" 
+cd() { builtin cd "$@"; ll; }               # Changement de dossier
+alias cd..="cd ../"                         # Retour en arrière rapide
+alias ..="cd ../"                           # Retour rapide 1 niveau
+alias ...="cd ../../"                       # Retour rapide 2 niveaux
+alias .3="cd ../../../"                     # Retour rapide 3 niveaux
+alias .4="cd ../../../../"                  # Retour rapide 4 niveaux
+alias .5="cd ../../../../../"               # Retour rapide 5 niveaux
+alias .6="cd ../../../../../../"            # Retour rapide 6 niveaux
+
+
+### ================================================================================================
+### Set ssh agent with key for every VM at startup
+if [ -f ~/.ssh/id_rsa ]; then
+
+  env=~/.ssh/agent_$(hostname).env
+  
+  agent_is_running() {
+    if [ "$SSH_AUTH_SOCK" ]; then
+      ssh-add -l >/dev/null 2>&1 || [ $? -eq 1 ]
+    else
+      false
+    fi
+  }
+  agent_has_keys() { ssh-add -l >/dev/null 2>&1; }
+  agent_load_env() { . "$env" >/dev/null; }
+  agent_start() { (umask 077; ssh-agent >"$env"); . "$env" >/dev/null; }
+  
+  if ! agent_is_running; then
+    agent_load_env
+  fi
+  
+  if ! agent_is_running; then
+    agent_start
+    ssh-add ~/.ssh/id_rsa
+  elif ! agent_has_keys; then
+    ssh-add ~/.ssh/id_rsa
+  fi
+
+  unset env
+  
+fi
+' > /media/User/$USER/.bash_profile && chown -R $USER:staff /media/User/$USER
+
